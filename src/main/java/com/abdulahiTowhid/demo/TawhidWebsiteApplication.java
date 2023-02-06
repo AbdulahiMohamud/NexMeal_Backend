c82507896e83cdb05b39f41da06fe27ba2867274
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2023-02-06 11:48:46 -0800

    initial commit to initialize git

---
+++ src/main/java/com/abdulahiTowhid/demo/TawhidWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.abdulahiTowhid.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TawhidWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TawhidWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdulahiTowhid/demo/TawhidWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulahiTowhid.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TawhidWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
