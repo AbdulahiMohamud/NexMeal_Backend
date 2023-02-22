735513bed9166df43184a918efb9614707a4bb66
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2023-02-21 20:59:42 -0800

    stater api works and allowed CORS for the react app to get the API

--- src/main/java/com/abdulahiTowhid/demo/TawhidWebsiteApplication.java
+++ src/main/java/com/abdulahiTowhid/demo/TawhidWebsiteApplication.java
@@ -2,12 +2,21 @@ package com.abdulahiTowhid.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TawhidWebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TawhidWebsiteApplication.class, args);
+
+
+
 	}
+
+
+
 
 }
