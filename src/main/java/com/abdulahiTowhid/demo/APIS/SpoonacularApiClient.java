d349c9086918122ddcc5a6262247948be0342045
Abdulahi Mohamud <abdulahimohamud22@gmail.com>
2023-03-04 19:19:41 -0800

    food api code is done but not complete yet need to debug it to find problem

---
+++ src/main/java/com/abdulahiTowhid/demo/APIS/SpoonacularApiClient.java
@@ -0,0 +1,41 @@
+package com.abdulahiTowhid.demo.APIS;
+
+import com.google.gson.Gson;
+import lombok.Data;
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.naming.directory.SearchResult;
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class SpoonacularApiClient {
+    @Value("${spoonacular.api.key}")
+    private static String API_KEY;
+    private static final String BASE_URL = "https://api.spoonacular.com/recipes/";
+
+
+    private final OkHttpClient client = new OkHttpClient();
+    private final Gson gson = new Gson();
+
+    public List<Recipe> searchRecipes(String query) throws IOException{
+        String url = BASE_URL + "complexSearch?apiKey=" + API_KEY + "&query=" + query;
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try(Response response = client.newCall(request).execute()) {
+            String responseBody = response.body().string();
+            SearchResults searchResult = gson.fromJson(responseBody,SearchResults.class);
+            return searchResult.getResults();
+
+        }
+    }
+    @Data
+    private static class SearchResults{
+
+        private List<Recipe> results;
+
+    }
+}
